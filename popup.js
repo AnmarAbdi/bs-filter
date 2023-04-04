f3a335c61364a72eb66ddf461493969776a8d844
Anmar <anmarabdi@yahoo.com>
2023-04-04 13:22:25 -0400

    anotha 1

--- popup.js
+++ popup.js
@@ -3,31 +3,30 @@ document.addEventListener("DOMContentLoaded", async function () {
 	  const tabId = tabs[0].id;
   
 	  chrome.tabs.executeScript(
+		tabId,
 		{
-		  target: { tabId: tabId },
-		  function: () => window.getSelection().toString(),
+		  code: "window.getSelection().toString();",
 		},
 		async (result) => {
-		  let highlightedText = result[0].result;
+		  let highlightedText = result[0];
 		  document.getElementById("highlighted-text").innerText = highlightedText;
 		  await getDefinition(highlightedText);
 		}
 	  );
 	});
-});
-
-async function getDefinition(text) {
-    const response = await fetch("https://bsproxy.herokuapp.com/get-definition", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text }),
-    });
-
-    const data = await response.json();
-    const definition = data.definition;
-    document.getElementById("definition").innerText = definition;
-}
-
+  });
+  
+  async function getDefinition(text) {
+	const response = await fetch("https://bsproxy.herokuapp.com/get-definition", {
+	  method: "POST",
+	  headers: {
+		"Content-Type": "application/json",
+	  },
+	  body: JSON.stringify({ text }),
+	});
+  
+	const data = await response.json();
+	const definition = data.definition;
+	document.getElementById("definition").innerText = definition;
+  }
   
