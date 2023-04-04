ff2064329a0cde4164f9e3227eff7423c675460d
Anmar <anmarabdi@yahoo.com>
2023-04-04 13:12:10 -0400

    Connected to huroku proxy server

--- popup.js
+++ popup.js
@@ -1,38 +1,33 @@
 document.addEventListener("DOMContentLoaded", async function () {
-	chrome.tabs.executeScript(
-	  {
-		code: "window.getSelection().toString();",
-	  },
-	  async function (result) { 
-		let highlightedText = result[0];
-		document.getElementById("highlighted-text").innerText = highlightedText;
-		await getDefinition(highlightedText);
-	  }
-	);
-  });
+	chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+	  const tabId = tabs[0].id;
   
-async function getDefinition(text) { 
-	const apiKey = "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";
-	const prompt = `Remove any and only biases and slanted language from the following article. Keep the exact article structure, and make sure to keep all context needed for the reader. Make sure to keep all quotes unchanged in the article: ${text}`;
-  
-	const response = await fetch("https://api.openai.com/v1/completions", {
-	  method: "POST",
-	  headers: {
-		"Content-Type": "application/json",
-		"Authorization": `Bearer ${apiKey}`
-	  },
-	  body: JSON.stringify({
-		model: "text-davinci-003",
-		prompt: prompt,
-		max_tokens: 1500,
-		n: 1,
-		stop: null,
-		temperature: 0.3,
-	  }),
+	  chrome.scripting.executeScript(
+		{
+		  target: { tabId: tabId },
+		  function: () => window.getSelection().toString(),
+		},
+		async (result) => {
+		  let highlightedText = result[0].result;
+		  document.getElementById("highlighted-text").innerText = highlightedText;
+		  await getDefinition(highlightedText);
+		}
+	  );
 	});
-  
-	const data = await response.json();
-	const definition = data.choices[0].text.trim();
-	document.getElementById("definition").innerText = definition;
+});
+
+async function getDefinition(text) {
+    const response = await fetch("https://bsproxy.herokuapp.com/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+    });
+
+    const data = await response.json();
+    const definition = data.definition;
+    document.getElementById("definition").innerText = definition;
 }
+
   
