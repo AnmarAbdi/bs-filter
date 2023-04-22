3b2e22d224624694f7d4e7f676d24e7c20484de0
Anmar <anmarabdi@yahoo.com>
2023-04-22 12:38:43 -0400

    chunks

--- popup.js
+++ popup.js
@@ -1,43 +1,82 @@
 document.addEventListener("DOMContentLoaded", async function () {
-	const getDefinitionButton = document.getElementById("get-definition-button");
-	getDefinitionButton.addEventListener("click", handleButtonClick);
+  const getDefinitionButton = document.getElementById("get-definition-button");
+  getDefinitionButton.addEventListener("click", handleButtonClick);
+});
+
+async function handleButtonClick() {
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    const tabId = tabs[0].id;
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        function: function () {
+          return window.getSelection().toString();
+        },
+      },
+      async (result) => {
+        let highlightedText = result[0].result;
+        const chunkSize = 350; // Set your desired chunk size
+
+        const definitionsElement = document.getElementById("definition");
+
+        if (highlightedText.length > chunkSize) {
+          const sentences = highlightedText.match(/[^.!?]+[.!?]+/g) || [
+            highlightedText,
+          ];
+          let chunks = [];
+          let currentChunk = "";
+
+          for (const sentence of sentences) {
+            if (currentChunk.length + sentence.length <= chunkSize) {
+              currentChunk += sentence;
+            } else {
+              chunks.push(currentChunk);
+              currentChunk = sentence;
+            }
+          }
+
+          if (currentChunk) {
+            chunks.push(currentChunk);
+          }
+
+          for (const chunk of chunks) {
+            const definition = await getDefinition(chunk);
+            if (definition) {
+              const newDefinitionDiv = document.createElement("div");
+              newDefinitionDiv.innerText = definition;
+              definitionsElement.appendChild(newDefinitionDiv);
+            }
+          }
+        } else {
+          const definition = await getDefinition(highlightedText);
+          if (definition) {
+            const newDefinitionDiv = document.createElement("div");
+            newDefinitionDiv.innerText = definition;
+            definitionsElement.appendChild(newDefinitionDiv);
+          }
+        }
+      }
+    );
   });
-  
-  async function handleButtonClick() {
-	chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-	  const tabId = tabs[0].id;
-  
-	  chrome.scripting.executeScript(
-		{
-		  target: { tabId: tabId },
-		  function: function() {
-			return window.getSelection().toString();
-		  },
-		},
-		async (result) => {
-		  let highlightedText = result[0].result;
-		  document.getElementById("highlighted-text").innerText = highlightedText;
-		  await getDefinition(highlightedText);
-		}
-	  );
-	});
-  }
-  
-  
-  async function getDefinition(text) {
-	const response = await fetch("https://bsproxy.herokuapp.com/get-definition", {
-	  method: "POST",
-	  headers: {
-		"Content-Type": "application/json",
-	  },
-	  body: JSON.stringify({ text }),
-	});
-  
-	const data = await response.json();
-	const definition = data.definition;
-	document.getElementById("definition").innerText = definition;
-  }
-  
+}
+
+async function getDefinition(text) {
+  const response = await fetch("https://bsproxy.herokuapp.com/get-definition", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+
+  const data = await response.json();
+  const definition = data.definition;
+  return definition;
+}
+
+/* 
+		+====- For a collapsable button -====+
   var coll = document.getElementsByClassName("collapsible");
   var i;
   
@@ -52,3 +91,4 @@ document.addEventListener("DOMContentLoaded", async function () {
 	  }
 	});
   }
+  */
